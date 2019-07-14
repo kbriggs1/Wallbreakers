b70a8bde4746aca36b38ae89cbda277381d2b401
kbriggs1 <49290565+kbriggs1@users.noreply.github.com>
2019-07-14 09:15:44 -0400

    Create Is-Sequence.py

---
+++ Week_Three/Is-Sequence.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+    
+        index_s, index_t = 0, 0
+        while index_s < len(s) and index_t < len(t):
+            if s[index_s] == t[index_t]:
+                index_s += 1
+            index_t += 1
+
+        return index_s == len(s)
